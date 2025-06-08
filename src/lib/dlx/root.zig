9be16dc98b960ac706dd0cff40e48694118708cd
Austin Bingham <austin.binghm@pm.me>
2025-06-08 10:54:43 +0200

    Restructured things a bit.

--- src/lib/dlx/root.zig
+++
@@ -1,5 +0,0 @@
-pub const matrix = @import("matrix.zig");
-
-test {
-    @import("std").testing.refAllDecls(@This());
-}
--- src/lib/matrix.zig
+++ src/lib/matrix.zig
@@ -1,38 +1,44 @@
+const dlx = @import("dlx.zig");
+const Node = dlx.Node;
 const std = @import("std");
 const testing = std.testing;
 const ArrayList = std.ArrayList;
-const Matrix = struct { nrows: usize, ncols: usize, data: ArrayList(u8) };
 
-// TODO: Try following the init/deinit pattern used by ArrayList
+const Matrix = struct {
+    header: []Node,
+    arena: std.heap.ArenaAllocator,
 
-pub fn make_matrix(nrows: usize, ncols: usize, allocator: std.mem.Allocator) !Matrix {
-    var list = ArrayList(u8).init(allocator);
-    try list.resize(nrows * ncols);
-    return Matrix{ .nrows = nrows, .ncols = ncols, .data = list };
-}
+    pub fn init(num_cols: usize) !Matrix {
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        const nodes = try arena.allocator().alloc(Node, num_cols + 1);
+        var index: usize = 0;
+        while (index < num_cols) : (index += 1) {
+            nodes[index] = Node {
+                .up = &nodes[0],
+                .down = &nodes[0],
+                .left = &nodes[0],
+                .right = &nodes[0],
+                .column = &nodes[0],
+                .count = 0,
+                .id = 0,
+            };
+        }
+        return Matrix{
+            .header = nodes,
+            .arena = arena
+        };
+    }
 
-pub fn free_matrix(matrix: Matrix) void {
-    matrix.data.deinit();
-}
+    pub fn deinit(self: Matrix) void {
+        self.arena.deinit();
+    }
+};
 
-pub fn set(matrix: *Matrix, row: usize, col: usize, value: u8) void {
-    const offset = row * matrix.ncols + col;
-    matrix.data.items[offset] = value;
-}
-
-test "basic make_matrix" {
-    const allocator = std.heap.page_allocator;
-    const matrix = try make_matrix(100, 100, allocator);
-    defer free_matrix(matrix);
-    try testing.expect(matrix.ncols == 100);
-    try testing.expect(matrix.nrows == 100);
-    try testing.expect(matrix.data.items.len == 10000);
-}
-
-test "set value" {
-    const allocator = std.heap.page_allocator;
-    var matrix = try make_matrix(100, 100, allocator);
-    defer free_matrix(matrix);
-    set(&matrix, 2, 2, 42);
-    try testing.expect(matrix.data.items[2 * matrix.ncols + 2] == 42);
+test "construct" {
+    const matrix = try Matrix.init(10);
+    defer matrix.deinit();
+    try testing.expect(true);
+    // try testing.expect(matrix.ncols == 100);
+    // try testing.expect(matrix.nrows == 100);
+    // try testing.expect(matrix.data.items.len == 10000);
 }
